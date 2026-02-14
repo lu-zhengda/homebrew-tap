bb9ae491f14874606d00c37eca89b0f42a4cc044
Zhengda Lu <zhengda.lu@datadoghq.com>
2026-02-14 17:08:07 -0500

    updater v0.3.0

--- Formula/updater.rb
+++ Formula/updater.rb
@@ -1,8 +1,8 @@
 class Updater < Formula
   desc "macOS app update manager"
   homepage "https://github.com/lu-zhengda/updater"
-  url "https://github.com/lu-zhengda/updater/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "d2d784d2acf4b08905611abfe3f5015214b2467dd7c409755a2c9bd1f75825e2"
+  url "https://github.com/lu-zhengda/updater/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "12b7f29c63cc401bb1cdee74405c19ec1dbad73d993d1760fee29493101a598f"
   license "MIT"
 
   depends_on "go" => :build
