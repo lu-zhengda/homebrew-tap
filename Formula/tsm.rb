e40b9ab44c2c08ee0e3f4d8777b84d9b8db26cd4
Zhengda Lu <zhengda.lu@datadoghq.com>
2026-03-28 18:22:20 -0400

    feat: add tsm formula

---
+++ Formula/tsm.rb
@@ -0,0 +1,29 @@
+class Tsm < Formula
+  desc "A CLI for Transmission BitTorrent client"
+  homepage "https://github.com/lu-zhengda/tsm"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-aarch64-apple-darwin.tar.gz"
+    else
+      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-x86_64-apple-darwin.tar.gz"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-aarch64-unknown-linux-gnu.tar.gz"
+    else
+      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-x86_64-unknown-linux-gnu.tar.gz"
+    end
+  end
+
+  def install
+    bin.install "tsm"
+  end
+
+  test do
+    assert_match "tsm", shell_output("#{bin}/tsm --version")
+  end
+end
