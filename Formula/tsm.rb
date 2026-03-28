785ed4fb596bb54940513f18e6faca5b4f5b8f99
Zhengda Lu <zhengda.lu@datadoghq.com>
2026-03-28 19:34:30 -0400

    fix: add version to tsm formula

--- Formula/tsm.rb
+++ Formula/tsm.rb
@@ -1,21 +1,22 @@
 class Tsm < Formula
   desc "A CLI for Transmission BitTorrent client"
   homepage "https://github.com/lu-zhengda/tsm"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/lu-zhengda/tsm/releases/download/v#{version}/tsm-aarch64-apple-darwin.tar.gz"
     else
-      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/lu-zhengda/tsm/releases/download/v#{version}/tsm-x86_64-apple-darwin.tar.gz"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/lu-zhengda/tsm/releases/download/v#{version}/tsm-aarch64-unknown-linux-gnu.tar.gz"
     else
-      url "https://github.com/lu-zhengda/tsm/releases/latest/download/tsm-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/lu-zhengda/tsm/releases/download/v#{version}/tsm-x86_64-unknown-linux-gnu.tar.gz"
     end
   end
 
