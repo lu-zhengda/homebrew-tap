d5d0a0f3fb353d745e3bf95aa78bada8fc57fd71
Zhengda Lu <zl2301@columbia.edu>
2026-07-15 12:15:06 -0400

    cronharbor: rely on URL version

--- Formula/cronharbor.rb
+++ Formula/cronharbor.rb
@@ -2,7 +2,6 @@ class Cronharbor < Formula
   desc "Native macOS menu bar app for safely managing user cron jobs"
   homepage "https://github.com/lu-zhengda/CronHarbor"
   url "https://github.com/lu-zhengda/CronHarbor/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
   sha256 "2e4b6d69d0c56fb2b6d1593c03ab624ff717582c5a3b93c24d2c80f057cdd18c"
   license "MIT"
 
