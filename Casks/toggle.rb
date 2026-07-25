d1f011db78ae71acd31da47ec9daf472fa784b41
Zhengda Lu <zhengda.lu@datadoghq.com>
2026-07-25 13:32:14 -0400

    toggle 1.4.0

--- Casks/toggle.rb
+++ Casks/toggle.rb
@@ -1,6 +1,6 @@
 cask "toggle" do
-  version "1.3.1"
-  sha256 "f9d834bee18c4d5b337023bacfcab3a1d85a53822c2328bbf183b12c6bb063bc"
+  version "1.4.0"
+  sha256 "062823247c4ae95ae28f2346a897f1aff01bcc4e349de3b761268d037d6e0e4f"
 
   url "https://github.com/lu-zhengda/toggle/releases/download/v#{version}/Toggle.zip"
   name "Toggle"
@@ -20,5 +20,8 @@ cask "toggle" do
     system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/Toggle.app"]
   end
 
-  zap trash: "~/Library/Preferences/com.local.toggle.plist"
+  zap trash: [
+    "~/Library/Preferences/com.local.toggle.plist",
+    "~/Library/Preferences/com.zhengdalu.toggle.plist",
+  ]
 end
