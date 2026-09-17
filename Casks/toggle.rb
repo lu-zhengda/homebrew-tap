2ec8311af85f1a4a9d798e8d655b4feaed219b71
Zhengda Lu <zhengda.lu@datadoghq.com>
2026-09-17 14:08:25 -0400

    Fix Toggle install steps for Homebrew 7

--- Casks/toggle.rb
+++ Casks/toggle.rb
@@ -17,7 +17,7 @@ cask "toggle" do
   app "Toggle.app"
 
   postflight_steps do
-    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/Toggle.app"]
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{staged_path}}/Toggle.app"]
   end
 
   zap trash: [
